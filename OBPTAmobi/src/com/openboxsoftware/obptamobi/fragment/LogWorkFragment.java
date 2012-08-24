b323507d1f299a33b82e2c5b72388ade080aa83a
zacblazic <zacblazic@gmail.com>
2012-08-24 12:22:30 +0200

    Fixing version 2.

---
+++ OBPTAmobi/src/com/openboxsoftware/obptamobi/fragment/LogWorkFragment.java
@@ -0,0 +1,61 @@
+package com.openboxsoftware.obptamobi.fragment;
+
+import com.openboxsoftware.obptamobi.R;
+import com.openboxsoftware.obptamobi.dialog.AddNewCategoryDialogFragment;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LogWorkFragment extends Fragment
+{
+	
+	
+	public LogWorkFragment() {
+		
+	}
+
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) 
+    {
+        View view;
+    	
+    	view = inflater.inflate(R.layout.fragment_log_work, container, false);
+    	
+    	Button newCategoryButton = (Button)view.findViewById(R.id.button_new_category);
+		newCategoryButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View view) {
+				showAddCategoryDialog();
+			}
+		});
+    	
+    	return view;
+    }
+    
+
+	private void showAddCategoryDialog() {
+		FragmentManager fm = getFragmentManager();
+    	FragmentTransaction ft = fm.beginTransaction();
+    	Fragment previous = fm.findFragmentByTag("add_new_category_dialog");
+    	
+    	if(previous != null) {
+    		ft.remove(previous);
+    	}
+    	
+    	ft.addToBackStack(null);
+    	
+    	DialogFragment fragment = AddNewCategoryDialogFragment.newInstance();
+    	fragment.show(ft, "add_new_category_dialog");
+	}
+}
