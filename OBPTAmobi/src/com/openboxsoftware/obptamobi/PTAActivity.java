2d56ff6f868e80d6cc8f2007c4c5be1cc077b976
zacblazic <zacblazic@gmail.com>
2012-08-24 16:05:19 +0200

    Added sign-in configuration.

--- OBPTAmobi/src/com/openboxsoftware/obptamobi/PTAActivity.java
+++ OBPTAmobi/src/com/openboxsoftware/obptamobi/PTAActivity.java
@@ -1,29 +1,36 @@
 package com.openboxsoftware.obptamobi;
 
+import java.io.File;
+import java.io.IOException;
+
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.widget.Toast;
 
+import com.openboxsoftware.obptamobi.authentication.Configuration;
+import com.openboxsoftware.obptamobi.authentication.ConfigurationHandler;
 import com.openboxsoftware.obptamobi.dialog.SignInDialogFragment;
 import com.openboxsoftware.obptamobi.fragment.LogWorkFragment;
 import com.openboxsoftware.obptamobi.fragment.SummaryFragment;
 
 public class PTAActivity extends FragmentActivity {
-
-    private static final String SIGNED_IN_STATE = "signed_in_state";
+    
+	public static final String STORAGE_PATH = Environment.getExternalStorageDirectory().getPath();
+	private static final String SIGNED_IN_STATE = "signed_in_state";
 	
 	private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
     private boolean signedIn;
-    
     private DialogFragment signInDialog;
-
+    private Configuration configuration;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
@@ -35,6 +42,12 @@ public class PTAActivity extends FragmentActivity {
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
   
+        if(!configurationExists()) {
+        	configuration = getDefaultConfiguration();
+        } else {
+        	configuration = getConfigurationFromFile();
+        }
+        
         signInDialog = new SignInDialogFragment();
         
         if(savedInstanceState != null) {
@@ -47,8 +60,7 @@ public class PTAActivity extends FragmentActivity {
     }
     
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) 
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_pta, menu);
         return true;
     }
@@ -120,5 +132,34 @@ public class PTAActivity extends FragmentActivity {
     public void showSignInDialog() {
     	FragmentManager fm = getSupportFragmentManager();
     	signInDialog.show(fm, "sign_in_dialog");
+    }
+    
+    private Configuration getConfigurationFromFile() {
+    	
+    	File file = new File(STORAGE_PATH + "/obpta/config");
+    	Configuration configuration = null;
+    	
+    	try {
+    		configuration = ConfigurationHandler.read(file);
+    	} catch(IOException ioe) {
+    		
+    	}
+    	
+    	return configuration;
+    }
+    
+    public Configuration getConfiguration() {
+    	return configuration;
+    }
+    
+    private boolean configurationExists() {
+    	File file = new File(STORAGE_PATH + "/obpta/config");
+    	return file.exists();
+    }
+    
+    private Configuration getDefaultConfiguration() {
+    	Configuration configuration = new Configuration();
+    	configuration.putOption("remember", "false");
+    	return configuration;
     }
 }
