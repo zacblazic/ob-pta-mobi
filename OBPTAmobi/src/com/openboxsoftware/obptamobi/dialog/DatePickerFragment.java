6cf939e78334e4cb831d67d5d2a116c9d5ee4328
gconry18 <garethc18@gmail.com>
2012-08-24 17:58:53 +0200

    Day of Week Changing correctly & spacing fixed

--- OBPTAmobi/src/com/openboxsoftware/obptamobi/dialog/DatePickerFragment.java
+++ OBPTAmobi/src/com/openboxsoftware/obptamobi/dialog/DatePickerFragment.java
@@ -10,13 +10,15 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
+	Calendar c;
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		
-		final Calendar c = Calendar.getInstance();
+		c = Calendar.getInstance();
 		int year = c.get(Calendar.YEAR);
 		int month = c.get(Calendar.MONTH);
 		int day = c.get(Calendar.DAY_OF_MONTH);
@@ -30,5 +32,13 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 	public void onDateSet(DatePicker view, int year, int month, int day) {
 		Button dateWorkedButton = (Button)getActivity().findViewById(R.id.button_date_worked);
 		dateWorkedButton.setText(day + "/" + month + "/" + year);
+		
+		String[] strDays = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thusday",
+		        "Friday", "Saturday" };
+		
+		c.set(year, month, day);
+		
+		TextView weekday = (TextView)getActivity().findViewById(R.id.label_weekday);
+		weekday.setText(strDays[c.get(Calendar.DAY_OF_WEEK) -1]);
     } 
 }
