61058d5cbaee3bf51c021d80f830d9fe5de9aa93
gconry18 <garethc18@gmail.com>
2012-08-24 11:16:25 +0200

    Removed Slider

--- OBPTAmobi/src/com/openboxsoftware/obptamobi/fragment/LogWorkFragment.java
+++ OBPTAmobi/src/com/openboxsoftware/obptamobi/fragment/LogWorkFragment.java
@@ -1,15 +1,18 @@
 package com.openboxsoftware.obptamobi.fragment;
 
-import com.openboxsoftware.obptamobi.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SlidingDrawer;
+
+import com.openboxsoftware.obptamobi.R;
 
 public class LogWorkFragment extends Fragment
 {
+	
+	SlidingDrawer drawer;
 	
 	
 	public LogWorkFragment() {
@@ -22,12 +25,10 @@ public class LogWorkFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) 
     {
-        /*TextView textView = new TextView(getActivity());
-        textView.setGravity(Gravity.CENTER);
-        Bundle args = getArguments();
-        textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
-        return textView;*/
+        View v;
+    	v = inflater.inflate(R.layout.fragment_log_work, container, false);
     	
-    	return inflater.inflate(R.layout.fragment_log_work, container, false);
+    	    	
+    	return v;
     }
 }
